7f061c7c9037a0886bde2dc9ba1c6ca191105956
TinBryn <griffk04@gmail.com>
2019-10-27 20:46:07 +0800

    Cleaned up getting port and removed jester dependency

--- src/development_tracker.nim
+++ src/development_tracker.nim
@@ -3,11 +3,11 @@ import htmlgen, os, strutils, asynchttpserver, asyncdispatch
 const
   portEnv = "PORT"
 
-var port: int
-if existsEnv(portEnv):
-  port = getEnv(portEnv).parseInt()
-else:
-  quit("can't find $" & portEnv)
+let port =
+  if existsEnv(portEnv):
+    getEnv(portEnv).parseInt()
+  else:
+    quit("can't find $" & portEnv)
 
 proc cb(req: Request) {.async.} =
   case req.url.path:
