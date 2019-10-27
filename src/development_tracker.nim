8a854e018e1d4ab87340049f7eed17f7e40348a4
TinBryn <griffk04@gmail.com>
2019-10-27 20:09:03 +0800

    Try again without a framework

--- src/development_tracker.nim
+++ src/development_tracker.nim
@@ -1,15 +1,17 @@
-import jester, htmlgen, os, strutils
-
-var settings = newSettings()
+import htmlgen, os, strutils, asynchttpserver, asyncdispatch
 
 const
   portEnv = "PORT"
   defaultPort = $8080
+  port = parseInt(getEnv(portEnv, defaultPort))
 
-settings.port = Port(parseInt(getEnv(portEnv, defaultPort)))
+proc cb(req: Request) {.async.} =
+  case req.url.path:
+  of "/":
+    await req.respond(Http200, h1("No Framework"))
+  else:
+    await req.respond(Http404, "Not found")
 
-routes:
-  get "/":
-    resp h1("Development Tracker")
+var server = newAsyncHttpServer()
 
-runForever()
+waitFor server.serve(Port(port), cb, "")
