cbe2d82a6437e95b756a3974ae5f755026b30385
TinBryn <griffk04@gmail.com>
2019-11-25 09:33:28 +0800

    Added router type

--- src/routes/router.nim
+++ src/routes/router.nim
@@ -1,5 +1,18 @@
 import tables, asynchttpserver, asyncdispatch
 
 type
+  Handle = proc(req: Request): Future[void]
   Router* = object
-    routes: Table[string, proc(req: Request): Future[void]]
+    routes: Table[string, Handle]
+    notFound: Handle
+
+proc initRouter*(notFound: Handle): Router =
+  result = Router(
+    routes: initTable[string, Handle](),
+    notFound: notFound)
+
+proc findHandle*(router: Router, path: string): Handle =
+  router.routes.getOrDefault(path, router.notFound)
+
+proc addHandle*(router: var Router, path: string, handle: Handle) =
+  router.routes[path] = handle
